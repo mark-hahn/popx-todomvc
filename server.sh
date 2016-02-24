5297f2bc009e6bfe104e393d52fab3e4d10dbddd
Mark Hahn <mark@hahnca.com>
2016-02-24 15:06:38 -0800

    added yaml parse

--- server.sh
+++ server.sh
@@ -2,6 +2,6 @@
 # script to compile popx todomvc web app and start serving it
 
 echo "compiling todomvc.popx"
-popx -xco js src/todomvc.popx
+node /root/dev/apps/popx/compiler/popx.js -xco js src/todomvc.popx
 
 http-server . -p 1999 -o -r
