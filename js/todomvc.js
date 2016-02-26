4b83c09af564f19e91fec4f191d01852e93a6d9f
Mark Hahn <mark@hahnca.com>
2016-02-26 12:59:33 -0800

    new api

--- js/todomvc.js
+++ js/todomvc.js
@@ -1,19 +1,15 @@
-
 /*{ name: 'todomvc',
   description: 'A popx todomvc example for todomvc.com',
   author: 'Mark Hahn <mark@hahnca.com>',
   repository: 'mark-hahn/popx-todomvc',
   file: 'src/todomvc.popx',
-  compiled: '2016-02-25 20:54:52' }*/
+  compiled: '2016-02-26 12:58:31' }*/
 
-(_=>{  
-  "use strict";
-  let env = JSON.parse(`
-    {"modules":{"newTodoInp":{"module":"stdlib/textInput","pins":{"selector":"_const0",
-    "changed":"addTodo"}},"log":{"module":"stdlib/log","pins":{"addTodo":"addTodo"}},
-    "_const0":{"module":"stdlib/constant","state":".new-todo","pins":{"out":"_const0"}}}}`);
-  let stdlibPath = require("popx-stdlib");
-  new(require(stdlibPath + "textInput"))(env, "newTodoInp", env.modules.newTodoInp);
-  new(require(stdlibPath + "log"))(env, "log", env.modules.log);
-  new(require(stdlibPath + "constant"))(env, "_const0", env.modules._const0);
-})();
+var env = JSON.parse(`
+    {"modules":[{"name":"log","type":"log","pins":{"addTodo":"_const0"}},{"name":"_const0",
+    "type":"constant","state":"asdf","pins":{"out":"_const0"}}]}`);
+var stdlibPath = require("popx-stdlib");
+for (var mod of env.modules) {
+  var path = (mod.type.indexOf('/') >= 0 ? mod.type : stdlibPath + mod.type);
+  new(require(path))(mod);
+}
--- server.sh
+++ server.sh
@@ -2,9 +2,10 @@
 # script to compile popx todomvc web app and start serving it
 
 echo "compiling todomvc.popx"
+node /root/dev/apps/popx/compiler/popx.js -co js src/todomvc.popx
 
-node /root/dev/apps/popx/compiler/popx.js -xco js src/todomvc.popx
 # node-debug --nodejs --harmony /root/dev/apps/popx/compiler/popx.js -xco js src/todomvc.popx
-
+# node-debug --nodejs --harmony /root/dev/apps/popx-todomvc/js/todomvc.js
+node --harmony /root/dev/apps/popx-todomvc/js/todomvc.js
 
 # http-server . -p 1999 -o -r
