3195ae9a8adc1d1df316965a7aa9fc17c5279d66
Mark Hahn <mark@hahnca.com>
2016-02-28 12:51:04 -0800

    coded all popx code

--- js/todomvc.js
+++ js/todomvc.js
@@ -3,9 +3,32 @@
   author: 'Mark Hahn <mark@hahnca.com>',
   repository: 'mark-hahn/popx-todomvc',
   file: 'src/todomvc.popx',
-  compiled: '2016-02-27 19:27:44' }*/
+  compiled: '2016-02-27 20:55:40' }*/
 
 var Popx = require('popx');
+
+var $textInput = null;
+(_=>{
+  'use strict';
+  $textInput = class extends Popx {
+    constructor (module) {
+      super(module);
+      
+      this.react('selector', (_, selector) => {
+        if (this.changeListener) {
+          this.ele.removeEventListener('change', this.changeListener);
+          delete this.changeListener;
+        }
+        if (selector) {
+          this.ele = document.querySelector(selector);
+          this.changeListener = e => this.emit('change', e.target.value);
+          this.ele.addEventListener('change', this.changeListener);
+        }
+      });
+      
+    }
+  };
+})();
 
 var fs     = require('fs');
 var util   = require('util');
@@ -19,37 +42,28 @@ var $log = null;
     constructor (module) {
       super(module);
       this.react( '*', (pinName, val, oldVal, sentFrom) => {
-        let line = valstr => `${moment().format().slice(0,-6).replace('T',' ')} 
-                              ${sentFrom.module}(${sentFrom.pinName}) 
-                              ->
-                              ${sentFrom.event ? 'event' : 'value'}
-                              wire:${sentFrom.wireName}, value:${valStr}
-                              -> ${this.module.name}(${pinName})`
-                              .replace(/\s+/g, ' ');
-        let valStr = util.inspect(val);
+        let line = `${moment().format().slice(0,-6).replace('T',' ')} 
+                    ${sentFrom.pinName}(${sentFrom.module}) 
+                    ${sentFrom.event ? 'event' : ''}
+                    ->
+                    ${sentFrom.wireName}: ${util.inspect(val)}`
+                    .replace(/\s+/g, ' ');
         if (this.get('console') !== false) {
-          console.log(line(valStr.replace(/\s+/g, ' ').slice(0,40)));
+          console.log(line.slice(0,100));
         }
         let path = this.get('path');
-        if (path) fs.appendFileSync(path, line(valStr)+'\n');
+        if (path) {
+          if (typeof fs.appendFileSync !== 'function') {
+            console.log('popx: no file sys to write log:', path);
+          } else fs.appendFileSync(path, line + '\n');
+        }
       });
     }
   };
   
 })();
 
-var $constant = null;
-(_=>{
-  'use strict';
-  $constant = class extends Popx {
-    constructor (module) {
-      super(module);
-      setTimeout((_=> this.set('out', module.state)), 0);
-    }
-  };
-})();
-
 var mods = [];
-mods.push(new($log)({"name":"log","type":"$log","pins":{"addTodo":"$const0"}}));
-mods.push(new($constant)({"name":"$const0","type":"$constant","state":23,"pins":{"out":"$const0"}}));
+mods.push(new($textInput)({"name":"newTodoInp","type":"$textInput","constants":{"selector":".new-todo"},"pins":{}}));
+mods.push(new($log)({"name":"log","type":"$log","constants":{"console":false},"pins":{}}));
 Popx.runLoop(mods);
--- server.sh
+++ server.sh
@@ -1,20 +1,23 @@
 
+# echo "compile todomvc with no debug"
+# node --harmony /root/dev/apps/popx/compiler/popx.js -co js src/todomvc.popx
+
 # echo "compile todomvc with debug"
 # node-debug --nodejs --harmony /root/dev/apps/popx/compiler/popx.js -co js src/todomvc.popx
 
-echo "compile todomvc and run in node with no debug"
-node /root/dev/apps/popx/compiler/popx.js -co js src/todomvc.popx
-node --harmony /root/dev/apps/popx-todomvc/js/todomvc.js
+# echo "compile todomvc and run in node with no debug"
+# node /root/dev/apps/popx/compiler/popx.js -co js src/todomvc.popx
+# node --harmony /root/dev/apps/popx-todomvc/js/todomvc.js
  
 # echo "compile todomvc and run in node with debug"
 # node /root/dev/apps/popx/compiler/popx.js -co js src/todomvc.popx
 # node-debug --nodejs --harmony /root/dev/apps/popx-todomvc/js/todomvc.js
-# 
+
 # echo "compile todomvc into bundle with debug"
 # node-debug --nodejs --harmony \
 # /root/dev/apps/popx/compiler/popx.js -o js -bmp js/bundle.js src/todomvc.popx
 
-# echo "compile todomvc into bundle and run in browser"
-# node /root/dev/apps/popx/compiler/popx.js -o js -bmp js/bundle.js src/todomvc.popx
-# http-server . -p 1999 -o -r
-
+echo "compile todomvc into bundle and run in browser"
+node /root/dev/apps/popx/compiler/popx.js -o js -bmp js/bundle.js src/todomvc.popx
+http-server . -p 1999 -o -r
+ 
